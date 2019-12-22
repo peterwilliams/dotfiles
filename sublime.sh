c6ece6c40659306e9a4405c9bb63e6fa2c856e8d
Peter Williams <peterwilliams@gmail.com>
2019-12-22 15:15:03 -0500

    Update Sublime Text preferences and packages
    I've migrated to VS Code for coding, so I don't need a lot of the packages that I used to for ST3 (which I still use as
    a basic text editor).

--- sublime.sh
+++ sublime.sh
@@ -5,4 +5,4 @@ DOTFILES_DIR="$HOME/development/code/personal/dotfiles"
 ln -sfv $DOTFILES_DIR/sublimetext/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -sfv $DOTFILES_DIR/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 ln -sfv $DOTFILES_DIR/sublimetext/ColorHighlighter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ColorHighlighter.sublime-settings
-ln -sfv $DOTFILES_DIR/sublimetext/SublimeLinter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
+ln -sfv $DOTFILES_DIR/sublimetext/OmniMarkupPreviewer.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/OmniMarkupPreviewer.sublime-settings
