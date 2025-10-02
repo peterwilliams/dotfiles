784e62aa3ce7b18fc2bcbc65db903836fb266d13
Peter Williams <peterwilliams@gmail.com>
2025-10-01 23:56:53 -0400

    Update Sublime Text installation directory in script

--- sublime.sh
+++ sublime.sh
@@ -2,7 +2,7 @@
 
 DOTFILES_DIR="$HOME/development/code/dotfiles"
 
-ln -sfv $DOTFILES_DIR/sublimetext/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -sfv $DOTFILES_DIR/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -sfv $DOTFILES_DIR/sublimetext/ColorHighlighter.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/ColorHighlighter.sublime-settings
-ln -sfv $DOTFILES_DIR/sublimetext/OmniMarkupPreviewer.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/OmniMarkupPreviewer.sublime-settings
+ln -sfv $DOTFILES_DIR/sublimetext/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -sfv $DOTFILES_DIR/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
+ln -sfv $DOTFILES_DIR/sublimetext/ColorHighlighter.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/ColorHighlighter.sublime-settings
+ln -sfv $DOTFILES_DIR/sublimetext/OmniMarkupPreviewer.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/OmniMarkupPreviewer.sublime-settings
